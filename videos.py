d957b58040998a9b708f567625bf36dbbe953456
Will Tekulve <tekulve.will@gmail.com>
2017-08-08 19:07:41 -0400

    More updates to pubsub/worker process

--- videos.py
+++ videos.py
@@ -46,6 +46,8 @@ def _extract_data_video_upload():
 def video_upload_with_overlay():
     overlay, (video, file_name, ext, content_type) = _extract_overlay_data()
 
+    print("extracted data")
+
     _publish_video_overlay_upload(overlay, video, file_name, ext, content_type)
 
     return '', 204
@@ -53,13 +55,16 @@ def video_upload_with_overlay():
 
 def _publish_video_overlay_upload(overlay: FileStorage, video: FileStorage,
                                   file_name: str, ext: str, content_type: str):
+    print('generating transcoding path')
     path = storage.generate_transcoding_path()
 
     overlay_path = path / 'overlay.png'
     video_path = path / (file_name + '.' + ext)
 
+    print('uploading data')
     storage.upload_data(overlay, 'image/png', overlay_path)
     storage.upload_data(video, content_type, video_path)
+    print('data uploaded')
 
     pubsub_payload = dict(
         video=str(video_path),
@@ -74,11 +79,10 @@ def _publish_video_overlay_upload(overlay: FileStorage, video: FileStorage,
 
 def _get_transcode_topic():
     client = pubsub.Client(
-        project=current_app.config['PROJECT_ID']
+        project=current_app.config['PROJECT_ID'],
+        _use_grpc=False
     )
     topic = client.topic(current_app.config['TRANSCODE_TOPIC'])
-    if not topic.exists():
-        topic.create()
     return topic
 
 
