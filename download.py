53b92bd2ed547674199492866e2dbaba59783538
Will Tekulve <tekulve.will@gmail.com>
2017-08-10 14:38:38 -0400

    Split download URLs

--- download.py
+++ download.py
@@ -2,7 +2,8 @@
 List of Endpoints
 =================
 
-    - :code:`/download`: Handled by :py:func:`.download`.
+    - :code:`/download/image`: Handled by :py:func:`.download_image`.
+    - :code:`/download/video`: Handled by :py:func:`.download_video`.
 
 API Reference
 =============
@@ -13,7 +14,7 @@ from flask import Flask, request, abort, redirect
 import storage
 
 
-def download():
+def download_image():
     """
     This function expects a :code:`key` URL parameter. Key is the post's ID and corresponds to the
     filename w/o extension in Storage. This endpoint should redirect to the file's location in Storage.
@@ -25,7 +26,26 @@ def download():
     if key is None:
         abort(400, 'Missing "key" URL parameter.')
 
-    file_url = storage.get_public_url(key)
+    file_url = storage.get_image_url(key)
+    if file_url is None:
+        abort(400, 'No matching file found.')
+
+    return redirect(file_url)
+
+
+def download_video():
+    """
+    This function expects a :code:`key` URL parameter. Key is the post's ID and corresponds to the
+    filename w/o extension in Storage. This endpoint should redirect to the file's location in Storage.
+
+    :return:
+        Redirects to the file's location in Storage.
+    """
+    key = request.args.get('key')
+    if key is None:
+        abort(400, 'Missing "key" URL parameter.')
+
+    file_url = storage.get_video_url(key)
     if file_url is None:
         abort(400, 'No matching file found.')
 
@@ -38,6 +58,9 @@ def setup_routing(app: Flask):
 
     :param flask.Flask app: Your flask application object.
     """
-    app.add_url_rule('/download', endpoint='download',
-                     view_func=download,
+    app.add_url_rule('/download/image', endpoint='download.image',
+                     view_func=download_image,
+                     methods=["GET"])
+    app.add_url_rule('/download/video', endpoint='download.video',
+                     view_func=download_video,
                      methods=["GET"])
